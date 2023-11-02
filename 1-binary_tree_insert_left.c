fc163a49ae244b59a6d1dec607914caa80936767
KwameNtaadu007 <kwamealabama@gmail.com>
2023-11-02 15:24:57 -0700

    refactored insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,22 +9,24 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-        return (NULL);
+	binary_tree_t *node = NULL;
 
-    binary_tree_t *node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-        return (NULL);
-
-    node->n = value;
-    node->parent = parent;
-    node->left = parent->left;
-    node->right = NULL;
-
-    if (parent->left)
-        parent->left->parent = node;
-    
-    parent->left = node;
-
-    return (node);
+	if (parent == NULL)
+		return (NULL);
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	if (parent->left == NULL)
+		parent->left = node;
+	else
+	{
+		node->left = parent->left;
+		parent->left = node;
+		node->left->parent = node;
+	}
+	return (node);
 }
